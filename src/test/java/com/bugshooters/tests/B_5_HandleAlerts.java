d11e99692653689cf242312fba337bf65a59dac8
Zafer Kok <zaferkok111@gmail.com>
2020-06-02 03:06:42 +0200

    11111

--- src/test/java/com/bugshooters/tests/B_5_HandleAlerts.java
+++ src/test/java/com/bugshooters/tests/B_5_HandleAlerts.java
@@ -10,15 +10,10 @@ import org.testng.annotations.Test;
 
 public class B_5_HandleAlerts extends TestBase {
 
+    /* Create 3 methods
+        1. acceptAlert    2. dismissAlert    3. sendKeysAlert
+    Go to https://the-internet.herokuapp.com/javascript_alerts */
 
-    /*
-Create 3 methods
-1. acceptAlert
-2. dismissAlert
-3. sendKeysAlert
-Create a @BeforeMethod
-Go to https://the-internet.herokuapp.com/javascript_alerts
- */
     AllPages allPages = new AllPages();
 
     @Test
@@ -27,7 +22,6 @@ Go to https://the-internet.herokuapp.com/javascript_alerts
         allPages.handleAlertsPage().clickbutton1.click();
         driver.switchTo().alert().accept();
         Assert.assertTrue(allPages.handleAlertsPage().resultmessage.isDisplayed());
-        System.out.println("Ali");
     }
     @Test
     public void testCase2() {
@@ -35,19 +29,15 @@ Go to https://the-internet.herokuapp.com/javascript_alerts
         allPages.handleAlertsPage().clickButton2.click();
         driver.switchTo().alert().dismiss();
         String expectedResultMessage="You clicked: Cancel";
-        Assert.assertEquals(allPages.handleAlertsPage().resultmessage2,expectedResultMessage);
-
+        Assert.assertEquals(allPages.handleAlertsPage().resultmessage2.getText(),expectedResultMessage);
     }
     @Test
     public void testCase3() throws InterruptedException {
         driver.get("https://the-internet.herokuapp.com/javascript_alerts");
         allPages.handleAlertsPage().clickButton3.click();
         driver.switchTo().alert().sendKeys("Ahmet");
-        Thread.sleep(5000);
         driver.switchTo().alert().accept();
-
         String expectedResultMessagelast="You entered: Ahmet";
         Assert.assertEquals(allPages.handleAlertsPage().resultmessage3.getText(),expectedResultMessagelast);
-
     }
 }
