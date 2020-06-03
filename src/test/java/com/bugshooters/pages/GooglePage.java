d593fdf844a05b74578c631ff75f7d19b81caa15
Zafer Kok <zaferkok111@gmail.com>
2020-06-03 23:49:36 +0200

    actions

--- src/test/java/com/bugshooters/pages/GooglePage.java
+++ src/test/java/com/bugshooters/pages/GooglePage.java
@@ -5,13 +5,10 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class WindowHandlePage {
-
-    public WindowHandlePage () {
+public class GooglePage {
+    public GooglePage(){
         PageFactory.initElements(Driver.getDriver(), this);
     }
-    @FindBy(xpath = "//h3")
-    public WebElement text;
-    @FindBy(xpath = "//a[@href='/windows/new']")
-    public WebElement button;
+    @FindBy (name = "q")
+    public WebElement searchBox;
 }
---
+++ src/test/java/com/bugshooters/tests/B_12_EnableTest.java
@@ -0,0 +1,4 @@
+package com.bugshooters.tests;
+
+public class B_12_EnableTest {
+}
